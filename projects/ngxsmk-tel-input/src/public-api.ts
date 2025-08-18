ee08c2de96894836a666758631110ab6178d51cc
Sachin Dilshan <sachin.d@wemakeplatforms.io>
2025-08-18 22:41:49 +0530

    code changes

--- projects/ngxsmk-tel-input/src/public-api.ts
+++ projects/ngxsmk-tel-input/src/public-api.ts
@@ -1,3 +1,3 @@
-export * from './lib/ngxsmk-tel-input.component';
-export * from './lib/ngxsmk-tel-input.service';
-export * from './lib/types';
+export { NgxsmkTelInputComponent } from './lib/ngxsmk-tel-input.component';
+export { NgxsmkTelInputService } from './lib/ngxsmk-tel-input.service';
+export type { IntlTelI18n, CountryMap } from './lib/types';
